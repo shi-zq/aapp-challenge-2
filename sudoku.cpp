6398629239f7d0b3e8fd967566bc27c90e62b444
shi-zq <76257648+shi-zq@users.noreply.github.com>
2024-11-17 14:47:59 +0100

    now trying with recursive method
    This reverts commit d2045dd046a9fbbc492b491bbcad55b16e205568.

--- sudoku.cpp
+++ sudoku.cpp
@@ -28,6 +28,114 @@
 
 #include "SudokuBoard.h"
 
+bool check(CSudokuBoard* tmpSudoku1) {	
+	int fieldSize = tmpSudoku1->getFieldSize();
+	int blockSize = tmpSudoku1->getBlockSize();
+	int* counter = new int[fieldSize];
+	for(int i = 0;  i < fieldSize; i++) {
+		counter[i] = 0;
+	}
+	//check row						
+	for(int i = 0; i < fieldSize; i++) {
+		for(int j = 0; j < fieldSize; j++) {
+			counter[tmpSudoku1->get(i, j)-1] = counter[tmpSudoku1->get(i, j)-1]+1;
+		}
+		for(int k = 0;  k < fieldSize; k++) {
+			if(counter[k] != 1) {
+				return false;
+			}
+			counter[k] = 0;
+		}
+	}							
+	//check column
+	for(int i = 0; i < fieldSize; i++) {
+		for(int j = 0; j < fieldSize; j++) {
+			counter[tmpSudoku1->get(j, i)-1] = counter[tmpSudoku1->get(j, i)-1]+1;
+		}
+		for(int k = 0;  k < fieldSize; k++) {
+			if(counter[k] != 1) {
+				return false;
+			}
+			counter[k] = 0;								
+		}
+	}			
+	//check block
+	for (int row = 0; row < blockSize; row++) {
+		for (int col = 0; col < blockSize; col++) {
+			for (int i = 0; i < blockSize; i++) {
+				for (int j = 0; j < blockSize; j++) {
+					counter[tmpSudoku1->get(row*blockSize + i, col*blockSize + j)-1] = counter[tmpSudoku1->get(row*blockSize + i, col*blockSize + j)-1]+1;
+				}
+			}
+			for(int k = 0;  k < fieldSize; k++) {
+				if(counter[k] != 1) {
+					return false;
+				}
+				counter[k] = 0;								
+			}
+		}
+	}
+	return true;
+}
+
+bool valid(CSudokuBoard* tmpSudoku1, int row, int col, int num) {
+	int fieldSize = tmpSudoku1->getFieldSize();
+	int blockSize = tmpSudoku1->getBlockSize();
+	for(int i = 0; i < fieldSize; i++) {
+		if(tmpSudoku1->get(row, i) == num) {
+			return false;
+		}
+	}
+	for(int i = 0; i < fieldSize; i++) {
+		if(tmpSudoku1->get(i, col) == num) {
+			return false;
+		}
+	}
+	int startRow = row - (row % blockSize);
+	int startCol = col - (col % blockSize);
+	for(int i = 0; i < blockSize; i++) {
+		for (int j = 0; j < blockSize; j++ ) {
+			if(tmpSudoku1->get(i + startRow, j + startCol) == num) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+void solver(CSudokuBoard* tmpSudoku1, int row, int col, int lastRow, int lastCol, int found) {
+	#pragma omp parallel 
+	{
+		for(int i = row; i < tmpSudoku1->getFieldSize(); i++) {
+			for(int j = col; j < tmpSudoku1->getFieldSize(); j++) {
+				if(tmpSudoku1->get(i, j) == 0) {
+					for(int num = 0; num < tmpSudoku1->getFieldSize(); num++) {
+						#pragma omp task firstprivate(i, j, tmpSudoku1, num) shared(lastRow, lastCol, found)
+						{
+							CSudokuBoard copySudoku(*tmpSudoku1);
+							if(valid(&copySudoku, i, j, num)) {
+								copySudoku.set(i, j, num);
+								solver(&copySudoku, i, j, lastRow, lastCol, found);
+								#pragma omp critical
+								{
+									if(i == lastRow && j == lastCol && found == 0) {
+										found = 1;
+										std::cout << "found "<< omp_get_thread_num() << std::endl;
+										copySudoku.printBoard();
+									}
+								}
+							}
+						}
+
+					}
+				}
+			}
+		}
+		#pragma omp taskwait
+	}
+}
+
+
 void printSol(int* sol, int length) {
 	for(int i = 0; i < length; i++) {
 		std::cout << sol[i] << " ";
@@ -63,7 +171,23 @@ int main(int argc, char* argv[]) {
 
 		//initializng some var
 		// solve the Sudoku by finding (and printing) all solutions
+		int rowZero;
+		int colZero;
+		for(int i = 0; i < sudoku1->getFieldSize(); i++) {
+			for(int j = 0; j < sudoku1->getFieldSize(); j++) {
+				if(sudoku1->get(i, j) == 0) {
+					rowZero = i;
+					colZero = j;
+				}
+			}
+		}
 		t3 = omp_get_wtime();
+		//std::cout << rowZero << " " << colZero;
+		solver(sudoku1, 0, 0, rowZero, colZero, 0);
+
+
+
+		/* 9^50 sembra troppe combinazione da riuscire a completare tutto
 		int fieldSize = sudoku1->getFieldSize();
 		int blockSize = sudoku1->getBlockSize();
 		int zeros = 0;
@@ -97,7 +221,7 @@ int main(int argc, char* argv[]) {
 				if(solution[zeros] == 1) {
 					run = false;
 				}
-				#pragma omp task
+				#pragma omp task shared(*sudoku1)
 				{
 					if(run) {
 						//std::cout << "from " << omp_get_thread_num() << " ";
@@ -120,46 +244,6 @@ int main(int argc, char* argv[]) {
 						for(int i = 0;  i < fieldSize; i++) {
 							counter[i] = 0;
 						}		
-						//check row						
-						for(int i = 0; i < fieldSize; i++) {
-							for(int j = 0; j < fieldSize; j++) {
-								counter[tmpSudoku1->get(i, j)-1] = counter[tmpSudoku1->get(i, j)-1]+1;
-							}
-							for(int k = 0;  k < fieldSize; k++) {
-								if(counter[k] != 1) {
-									found = false;
-								}
-								counter[k] = 0;
-							}
-						}							
-						//check column
-						for(int i = 0; i < fieldSize; i++) {
-							for(int j = 0; j < fieldSize; j++) {
-								counter[tmpSudoku1->get(j, i)-1] = counter[tmpSudoku1->get(j, i)-1]+1;
-							}
-							for(int k = 0;  k < fieldSize; k++) {
-								if(counter[k] != 1) {
-									found = false;
-								}
-								counter[k] = 0;								
-							}
-						}			
-						//check block
-						for (int row = 0; row < blockSize; row++) {
-							for (int col = 0; col < blockSize; col++) {
-								for (int i = 0; i < blockSize; i++) {
-									for (int j = 0; j < blockSize; j++) {
-										counter[tmpSudoku1->get(row*blockSize + i, col*blockSize + j)-1] = counter[tmpSudoku1->get(row*blockSize + i, col*blockSize + j)-1]+1;
-									}
-								}
-								for(int k = 0;  k < fieldSize; k++) {
-									if(counter[k] != 1) {
-										found = false;
-									}
-									counter[k] = 0;								
-								}
-							}
-						}
 						if(found) {
 							std::cout << "found from " << omp_get_thread_num() << std::endl; 
 							tmpSudoku1->printBoard();
@@ -174,108 +258,7 @@ int main(int argc, char* argv[]) {
 			}
 			#pragma omp taskwait
 		}
-
-
-	    // TO BE IMPLEMENTED	
-		/*		
-		int fieldSize = sudoku1->getFieldSize();
-		int blockSize = sudoku1->getBlockSize();
-		int zeros = 0;
-		for(int i = 0; i < fieldSize; i++) {
-			for(int j = 0; j < fieldSize; j++) {
-				if(sudoku1->get(i, j) == 0) {
-					zeros = zeros + 1;
-				}
-			}
-		}
-		int* solution = new int[zeros+1];
-		bool cond = false;
-		solution[0] = 1;
-		for(int i = 1;  i < zeros+1; i++) {
-			solution[i] = 1;
-		}
-		#pragma omp parallel
-		{
-			#pragma omp single
-			{
-				while(cond) {
-					//generate the solution to be tested and copy to an new array
-					solution[0] = solution[0]+1;
-					int cont = 0;
-					for(int i = 0; i < zeros; i++) {
-						if(solution[i] > fieldSize) {
-							solution[i] = 1;
-							solution[i+1] = solution[i+1] + 1;
-						}
-					}
-					if(solution[zeros+1] == 2) {
-						cond = false;
-					}
-					#pragma omp task firstprivate(solution, sudoku1)
-					{
-						int index = 0;
-						bool found = true;
-						for(int i = 0; i < fieldSize; i++) {
-							for(int j = 0; j < fieldSize; j++) {
-								if(sudoku1->get(i, j) == 0) {
-									sudoku1->set(i, j, solution[index]);
-									index = index+1;
-								}
-							}
-						}
-						int* counter = new int[fieldSize];
-						for(int i = 0;  i < fieldSize; i++) {
-							counter[i] = 0;
-						}		
-						//check row						
-						for(int i = 0; i < fieldSize; i++) {
-							for(int j = 0; j < fieldSize; j++) {
-								counter[sudoku1->get(i, j)-1] = counter[sudoku1->get(i, j)-1]+1;
-							}
-							for(int k = 0;  k < fieldSize; k++) {
-								if(counter[k] != 1) {
-									found = false;
-								}
-								counter[k] = 0;
-							}
-						}							
-						//check column
-						for(int i = 0; i < fieldSize; i++) {
-							for(int j = 0; j < fieldSize; j++) {
-								counter[sudoku1->get(j, i)-1] = counter[sudoku1->get(j, i)-1]+1;
-							}
-							for(int k = 0;  k < fieldSize; k++) {
-								if(counter[k] != 1) {
-									found = false;
-								}
-								counter[k] = 0;								
-							}
-						}			
-						//check block
-						for (int row = 0; row < blockSize; row++) {
-							for (int col = 0; col < blockSize; col++) {
-								for (int i = 0; i < blockSize; i++) {
-									for (int j = 0; j < blockSize; j++) {
-										counter[sudoku1->get(row*blockSize + i, col*blockSize + j)-1] = counter[sudoku1->get(row*blockSize + i, col*blockSize + j)-1]+1;
-									}
-								}
-								for(int k = 0;  k < fieldSize; k++) {
-									if(counter[k] != 1) {
-										found = false;
-									}
-									counter[k] = 0;								
-								}
-							}
-						}
-						if(found) {
-							sudoku1->printBoard();
-						}
-					}
-				}
-			}
-		}
-	}*/
-
+		*/
 		t4 = omp_get_wtime();
 		
 		delete sudoku1;
